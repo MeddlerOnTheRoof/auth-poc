977dd4fd4150083a82ad3a2117d76ffd5e6454b1
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-24 15:17:05 -0400

    Add dbo.Unit scripts

---
+++ auth-poc.data/scripts/implementation/dbo.Unit_create.sql
@@ -0,0 +1,111 @@
+﻿/*
+Description:	CREATE Table dbo.Unit
+Author:			MeddlerOnTheRoof
+Date:			May 23, 2017
+*/
+
+begin try
+	use authpoc;
+
+	if not exists(
+		select 1 from information_schema.tables where 
+		table_schema = 'dbo' and table_name = 'Unit'
+	)
+		begin
+			create table dbo.Unit(
+				UnitId int identity(1,1) not null,
+				UnitName varchar(25) not null,
+				UnitTypeId int not null,
+				Food int not null,
+				Gold int not null,
+				Stone int not null,
+				Wood int not null,
+				BuildTime time not null default '00:00:00',
+				MoveSpeed int not null,
+				LineOfSight int not null,
+				Health int not null,
+				AttackRange int null,
+				Attack int not null,
+				AttackTypeId int not null,
+				AttackSpeed int not null,
+				CreatedByUser varchar(20) not null,
+				CreatedByDate datetime not null,
+				ModifiedByUser varchar(20) not null,
+				ModifiedByDate datetime not null,
+				constraint pk_Unit primary key (
+					UnitId asc
+				),
+				constraint uq_Unit_UnitName unique (
+					UnitName
+				)
+			)
+
+			print 'dbo.Unit CREATED';
+		end;
+	else
+		print 'dbo.Unit was already CREATED';
+
+	if not exists(
+		select 1 from information_schema.table_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_Unit_UnitType'
+	)
+		begin
+			alter table dbo.Unit with check add constraint fk_Unit_UnitType 
+			foreign key(UnitTypeId) references dbo.UnitType (UnitTypeId)
+
+			print 'dbo.fk_Unit_UnitType constraint CREATED';
+		end;
+	else
+		print 'dbo.fk_Unit_UnitType constraint NOT CREATED';
+
+	if exists(
+		select 1 from information_schema.table_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_Unit_UnitType'
+	)
+		begin
+			alter table dbo.Unit check constraint fk_Unit_UnitType;
+
+			print 'dbo.fk_Unit_UnitType check constraint altered';
+		end;
+	else
+		print 'dbo.fk_Unit_UnitType check constraint NOT altered';
+
+	if not exists(
+		select 1 from information_schema.table_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_Unit_AttackType'
+	)
+		begin
+			alter table dbo.Unit with check add constraint fk_Unit_AttackType 
+			foreign key(AttackTypeId) references dbo.AttackType (AttackTypeId)
+
+			print 'dbo.fk_Unit_AttackType constraint CREATED';
+		end;
+	else
+		print 'dbo.fk_Unit_AttackType constraint NOT CREATED';
+
+	if exists(
+		select 1 from information_schema.table_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_Unit_AttackType'
+	)
+		begin
+			alter table dbo.Unit check constraint fk_Unit_AttackType;
+
+			print 'dbo.fk_Unit_AttackType check constraint altered';
+		end;
+	else
+		print 'dbo.fk_Unit_AttackType check constraint NOT altered';
+end try
+
+begin catch
+	declare @errmsg nvarchar(2048);
+	declare @errsev int;
+	declare @errstate int;
+
+	set @errmsg = 'Error: ' + error_message();
+	set @errsev = error_severity();
+	set @errstate = error_state();
+
+	raiserror(@errmsg, @errsev, @errstate);
+end catch
+
+go
---
+++ auth-poc.data/scripts/rollback/dbo.Unit_drop.sql
@@ -0,0 +1,59 @@
+﻿/*
+Description:		DROP Table dbo.Unit
+Author:				MeddlerOnTheRoof
+Date:				May 24, 2017
+*/
+
+begin try
+	use authpoc;
+
+	if exists (
+		select * from information_schema.referential_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_Unit_UnitType'
+	)
+		begin
+			alter table dbo.Unit drop constraint fk_Unit_UnitType;
+
+			print 'dbo.fk_Unit_UnitType constraint DROPPED';
+		end
+	else
+		print 'dbo.fk_Unit_UnitType constraint was already DROPPED';
+
+	if exists (
+		select * from information_schema.referential_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_Unit_AttackType'
+	)
+		begin
+			alter table dbo.Unit drop constraint fk_Unit_AttackType;
+
+			print 'dbo.fk_Unit_AttackType constraint DROPPED';
+		end
+	else
+		print 'dbo.fk_Unit_AttackType constraint was already DROPPED';
+
+	if exists (
+		select * from information_schema.tables where
+		table_schema = 'dbo' and table_name = 'Unit'
+	)
+		begin
+			drop table dbo.Unit;
+
+			print 'dbo.Unit DROPPED';
+		end
+	else
+		print 'dbo.Unit was already DROPPED';
+end try
+
+begin catch
+	declare @errmsg nvarchar(2048);
+	declare @errsev int;
+	declare @errstate int;
+
+	set @errmsg = 'Error: ' + error_message();
+	set @errsev = error_severity();
+	set @errstate = error_state();
+
+	raiserror(@errmsg, @errsev, @errstate);
+end catch
+
+go
