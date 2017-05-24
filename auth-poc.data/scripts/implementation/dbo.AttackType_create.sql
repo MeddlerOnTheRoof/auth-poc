fd4a0c7cf8c1cb6124b70e972b086f36d438bd2d
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-24 15:15:46 -0400

    Add dbo.AttackType scripts

---
+++ auth-poc.data/scripts/implementation/dbo.AttackType_create.sql
@@ -0,0 +1,77 @@
+﻿/*
+Description:	CREATE Table dbo.AttackType
+Author:			MeddlerOnTheRoof
+Date:			May 23, 2017
+*/
+
+begin try
+	use authpoc;
+
+	if not exists(
+		select 1 from information_schema.tables where 
+		table_schema = 'dbo' and table_name = 'AttackType'
+	)
+		begin
+			create table dbo.AttackType(
+				AttackTypeId int identity(1,1) not null,
+				AttackTypeName varchar(25) not null,
+				ArmorTypeId int not null,
+				CreatedByUser varchar(20) not null,
+				CreatedByDate datetime not null,
+				ModifiedByUser varchar(20) not null,
+				ModifiedByDate datetime not null,
+				constraint pk_AttackType primary key (
+					AttackTypeId asc
+				),
+				constraint uq_AttackType_AttackTypeName unique (
+					AttackTypeName
+				),
+				constraint uq_AttackType_ArmorTypeId unique (
+					ArmorTypeId
+				)
+			)
+
+			print 'dbo.AttackType CREATED';
+		end;
+	else
+		print 'dbo.AttackType was already CREATED';
+
+	if not exists(
+		select 1 from information_schema.table_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_AttackType_ArmorType'
+	)
+		begin
+			alter table dbo.AttackType with check add constraint fk_AttackType_ArmorType 
+			foreign key(ArmorTypeId) references dbo.ArmorType (ArmorTypeId)
+
+			print 'dbo.fk_AttackType_ArmorType constraint CREATED';
+		end;
+	else
+		print 'dbo.fk_AttackType_ArmorType constraint NOT CREATED';
+
+	if exists(
+		select 1 from information_schema.table_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_AttackType_ArmorType'
+	)
+		begin
+			alter table dbo.AttackType check constraint fk_AttackType_ArmorType;
+
+			print 'dbo.fk_AttackType_ArmorType check constraint altered';
+		end;
+	else
+		print 'dbo.fk_AttackType_ArmorType check constraint NOT altered';
+end try
+
+begin catch
+	declare @errmsg nvarchar(2048);
+	declare @errsev int;
+	declare @errstate int;
+
+	set @errmsg = 'Error: ' + error_message();
+	set @errsev = error_severity();
+	set @errstate = error_state();
+
+	raiserror(@errmsg, @errsev, @errstate);
+end catch
+
+go
---
+++ auth-poc.data/scripts/rollback/dbo.AttackType_drop.sql
@@ -0,0 +1,47 @@
+﻿/*
+Description:		DROP Table dbo.AttackType
+Author:				MeddlerOnTheRoof
+Date:				May 24, 2017
+*/
+
+begin try
+	use authpoc;
+
+	if exists (
+		select * from information_schema.referential_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_AttackType_ArmorType'
+	)
+		begin
+			alter table dbo.AttackType drop constraint fk_AttackType_ArmorType;
+
+			print 'dbo.fk_AttackType_ArmorType constraint DROPPED';
+		end
+	else
+		print 'dbo.fk_AttackType_ArmorType constraint was already DROPPED';
+
+	if exists (
+		select * from information_schema.tables where
+		table_schema = 'dbo' and table_name = 'AttackType'
+	)
+		begin
+			drop table dbo.AttackType;
+
+			print 'dbo.AttackType DROPPED';
+		end
+	else
+		print 'dbo.AttackType was already DROPPED';
+end try
+
+begin catch
+	declare @errmsg nvarchar(2048);
+	declare @errsev int;
+	declare @errstate int;
+
+	set @errmsg = 'Error: ' + error_message();
+	set @errsev = error_severity();
+	set @errstate = error_state();
+
+	raiserror(@errmsg, @errsev, @errstate);
+end catch
+
+go
