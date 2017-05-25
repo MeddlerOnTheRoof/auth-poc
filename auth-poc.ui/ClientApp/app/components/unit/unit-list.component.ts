88f4acbbe6d4d7c53ff682ca180422f477d035ab
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-25 11:04:02 -0400

    Add unit-list.component

---
+++ auth-poc.ui/ClientApp/app/components/unit/unit-list.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component, OnInit, Injectable } from '@angular/core';
+import { Unit } from './unit';
+import { UnitService } from './unit.service';
+
+@Component({
+    selector: 'unit-list',
+    template: require('./unit-list.component.html'),
+    providers: [UnitService]
+})
+
+@Injectable()
+export class UnitListComponent implements OnInit {
+    private units: Unit[];
+
+    constructor(private unitService: UnitService) { }
+
+    public ngOnInit(): void {
+        this.loadUnits();
+    }
+
+    private delete(unitId: number): void {
+        this.unitService.deleteUnit(unitId)
+            .subscribe(() => {
+                this.loadUnits();
+            });
+    }
+
+    private loadUnits(): void {
+        this.unitService.getUnits()
+            .subscribe((units: Unit[]) => {
+                this.units = units;
+            });
+    }
+}
