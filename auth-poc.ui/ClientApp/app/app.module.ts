56970158fd845b263ed52125bf19690af6f0ef46
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-25 11:04:45 -0400

    Update app module and navmenu

--- auth-poc.ui/ClientApp/app/app.module.ts
+++ auth-poc.ui/ClientApp/app/app.module.ts
@@ -4,18 +4,26 @@ import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
+import { UnitService } from './components/unit/unit.service';
+import { UnitListComponent } from './components/unit/unit-list.component';
+import { UnitDetailComponent } from './components/unit/unit-detail.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
         NavMenuComponent,
+        HomeComponent,
+        UnitListComponent,
+        UnitDetailComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
+            { path: 'unit-list', component: UnitListComponent },
+            { path: 'unit-detail', component: UnitDetailComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
