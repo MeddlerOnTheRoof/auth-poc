ec782d7190b33f19ad1ce60647d0fae49e37ab05
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-25 10:55:46 -0400

    Add CivilizationsRepository

---
+++ auth-poc.data/Interfaces/ICivilizationsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using auth_poc.data.Models;
+
+namespace auth_poc.data.Interfaces
+{
+    public interface ICivilizationsRepository
+    {
+        Task<IEnumerable<Civilization>> GetCivilizationsAsync();
+        Task<Civilization> GetCivilizationAsync(int civilizationId);
+        Task<int> AddCivilizationAsync(Civilization civilization);
+        Task<int> UpdateCivilization(Civilization civilization);
+        Task<bool> IsExistingCivilization(int civilizationId);
+    }
+}
---
+++ auth-poc.data/Repositories/CivilizationsRepository.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using auth_poc.data.Interfaces;
+using auth_poc.data.Models;
+using EF = auth_poc.data.DAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace auth_poc.data.Repositories
+{
+    public class CivilizationsRepository : ICivilizationsRepository
+    {
+        private readonly EF.AuthPocContext _db;
+
+        public CivilizationsRepository()
+        {
+            _db = new EF.AuthPocContext();
+        }
+
+        public async Task<IEnumerable<Civilization>> GetCivilizationsAsync()
+        {
+            return (await _db.Civilization.ToListAsync()).Select(ToDomainModel);
+        }
+
+        public async Task<Civilization> GetCivilizationAsync(int civilizationId)
+        {
+            return ToDomainModel(await _db.Civilization.FirstOrDefaultAsync(c => c.CivilizationId == civilizationId));
+        }
+
+        public async Task<int> AddCivilizationAsync(Civilization civilization)
+        {
+            var model = ToDatabaseModel(civilization);
+
+            model.CreatedByUser = civilization.CreatedByUser;
+            model.CreatedByDate = DateTime.Now;
+
+            _db.Civilization.Add(model);
+
+            await _db.SaveChangesAsync();
+
+            return model.CivilizationId;
+        }
+
+        public async Task<int> UpdateCivilization(Civilization civilization)
+        {
+            var model = await _db.Civilization.FirstOrDefaultAsync(c => c.CivilizationId == civilization.CivilizationId);
+
+            if (model == null)
+                return 0;
+
+            model = ToDatabaseModel(civilization);
+
+            return await _db.SaveChangesAsync();
+        }
+
+        public async Task<bool> IsExistingCivilization(int civilizationId)
+        {
+            return await _db.Civilization.AnyAsync(c => c.CivilizationId == civilizationId);
+        }
+
+        private Civilization ToDomainModel(EF.Civilization civ)
+        {
+            return civ != null ? new Civilization() {
+                CivilizationName = civ.CivilizationName,
+                CivilizationDescription = civ.CivilizationDescription,
+                CreatedByUser = civ.CreatedByUser,
+                CreatedByDate = civ.CreatedByDate,
+                ModifiedByUser = civ.ModifiedByUser,
+                ModifiedByDate = civ.ModifiedByDate
+            } : null;
+        }
+
+        private EF.Civilization ToDatabaseModel(Civilization civ)
+        {
+            return civ != null ? new EF.Civilization() {
+                CivilizationName = civ.CivilizationName,
+                CivilizationDescription = civ.CivilizationDescription,
+                ModifiedByUser = civ.ModifiedByUser,
+                ModifiedByDate = DateTime.Now
+            } : null;
+        }
+    }
+}
