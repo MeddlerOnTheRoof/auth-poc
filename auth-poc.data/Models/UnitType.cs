d3bad42052e7b1217b49b3cd97bf023ec8c1cf30
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-25 10:54:56 -0400

    Add UnitType model

---
+++ auth-poc.data/Models/UnitType.cs
@@ -0,0 +1,8 @@
+﻿namespace auth_poc.data.Models
+{
+    public class UnitType : BaseEntity
+    {
+        public int UnitTypeId { get; set; }
+        public string UnitTypeName { get; set; }
+    }
+}
