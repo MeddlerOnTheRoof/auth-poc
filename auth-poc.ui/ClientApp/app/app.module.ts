1981b4b8d0aa7ea7d1c688772bd0f82b40c52e64
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-25 15:01:00 -0400

    Add FormsModule

--- auth-poc.ui/ClientApp/app/app.module.ts
+++ auth-poc.ui/ClientApp/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component'
@@ -19,6 +20,7 @@ import { UnitDetailComponent } from './components/unit/unit-detail.component';
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
