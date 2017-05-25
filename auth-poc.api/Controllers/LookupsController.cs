aa05981fbdc1117dcd24a4bf0e5297d5ed1d3b44
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-25 10:59:27 -0400

    Add LookupsController

---
+++ auth-poc.api/Controllers/LookupsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using auth_poc.data.Interfaces;
+using auth_poc.data.Models;
+using Swashbuckle.SwaggerGen.Annotations;
+
+namespace auth_poc.api.Controllers
+{
+    [Route("api/[controller]")]
+    public class LookupsController : Controller
+    {
+        private readonly ILookupsRepository _db;
+
+        public LookupsController(ILookupsRepository db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("UnitTypes")]
+        [SwaggerResponse(System.Net.HttpStatusCode.OK, Type = typeof(IEnumerable<UnitType>))]
+        public async Task<IActionResult> Get()
+        {
+            var unitTypes = await _db.GetUnitTypes();
+
+            return Ok(unitTypes);
+        }
+    }
+}
