83d06ff4f0dc34cee672596fc315d9b574bcf191
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-25 10:54:46 -0400

    Add BaseEntity model

---
+++ auth-poc.data/Models/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace auth_poc.data.Models
+{
+    public class BaseEntity
+    {
+        public string CreatedByUser { get; set; }
+        public DateTime CreatedByDate { get; set; }
+        public string ModifiedByUser { get; set; }
+        public DateTime ModifiedByDate { get; set; }
+    }
+}
