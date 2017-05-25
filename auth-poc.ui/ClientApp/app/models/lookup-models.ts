e12e47ba5fa2be8a6c4ba52f6c26b35325af2dc6
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-25 11:03:32 -0400

    Add lookup model and service

---
+++ auth-poc.ui/ClientApp/app/models/lookup-models.ts
@@ -0,0 +1,6 @@
+﻿import { BaseEntity } from './base-entity';
+
+export class UnitType extends BaseEntity {
+    public unitTypeId: number;
+    public unitTypeName: string;
+}
---
+++ auth-poc.ui/ClientApp/app/shared/lookup.service.ts
@@ -0,0 +1,25 @@
+﻿import { Http, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { UnitType } from '../models/lookup-models';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class LookupService {
+    private baseUrl: string = 'http://localhost:3301/api/Lookup/';
+
+    constructor(private http: Http) { }
+
+    private handleError(error: any): Promise<any> {
+        debugger;
+        console.error(error);
+        return Promise.reject(error.message || error);
+    }
+
+    public getUnitTypes(): Observable<UnitType[]> {
+        return this.http.get(this.baseUrl + 'UnitTypes')
+            .map((response: Response) => <UnitType[]>response.json())
+            .catch(this.handleError)
+    }
+}
