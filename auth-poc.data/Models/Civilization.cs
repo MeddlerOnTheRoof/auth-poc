8f74ce28fff5500c15f2b1c412bc25917a910205
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-25 10:55:16 -0400

    Add Civilization model

---
+++ auth-poc.data/Models/Civilization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace auth_poc.data.Models
+{
+    public class Civilization : BaseEntity
+    {
+        public int CivilizationId { get; set; }
+        public string CivilizationName { get; set; }
+        public string CivilizationDescription { get; set; }
+    }
+}
