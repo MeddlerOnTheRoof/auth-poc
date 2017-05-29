3d7ec83cb8c9ef527f52cbc3de0517558aee7ace
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-29 14:07:23 -0400

    Add UserAccount scripts

---
+++ auth-poc.data/scripts/implementation/dbo.UserAccount_create.sql
@@ -0,0 +1,74 @@
+﻿/*
+Description:	CREATE Table dbo.UserAccount
+Author:			MeddlerOnTheRoof
+Date:			May 25, 2017
+*/
+
+begin try
+	use authpoc;
+
+	if not exists(
+		select 1 from information_schema.tables where 
+		table_schema = 'dbo' and table_name = 'UserAccount'
+	)
+		begin
+			create table dbo.UserAccount(
+				UserAccountId int identity(1,1) not null,
+				UserAccountName varchar(25) not null,
+				UserAccountPassword varchar(16) not null,
+				UserRoleId int not null,
+				CreatedByUserAccount varchar(20) not null,
+				CreatedByDate datetime not null,
+				ModifiedByUserAccount varchar(20) not null,
+				ModifiedByDate datetime not null,
+				constraint pk_UserAccount primary key (
+					UserAccountId asc
+				),
+				constraint uq_UserAccount_UserAccountName unique (
+					UserAccountName
+				)
+			)
+			print 'dbo.UserAccount CREATED';
+		end;
+	else
+		print 'dbo.UserAccount was already CREATED';
+
+	if not exists(
+		select 1 from information_schema.table_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_UserAccount_UserRole'
+	)
+		begin
+			alter table dbo.UserAccount with check add constraint fk_UserAccount_UserRole 
+			foreign key(UserRoleId) references dbo.UserRole (UserRoleId)
+
+			print 'dbo.fk_UserAccount_UserRole constraint CREATED';
+		end;
+	else
+		print 'dbo.fk_UserAccount_UserRole constraint NOT CREATED';
+
+	if exists(
+		select 1 from information_schema.table_constraints where
+		constraint_schema = 'dbo' and constraint_name = 'fk_UserAccount_UserRole'
+	)
+		begin
+			alter table dbo.UserAccount check constraint fk_UserAccount_UserRole;
+
+			print 'dbo.fk_UserAccount_UserRole check constraint altered';
+		end;
+	else
+		print 'dbo.fk_UserAccount_UserRole check constraint NOT altered';
+end try
+
+begin catch
+	declare @errmsg nvarchar(2048);
+	declare @errsev int;
+	declare @errstate int;
+
+	set @errmsg = 'Error: ' + error_message();
+	set @errsev = error_severity();
+	set @errstate = error_state();
+
+	raiserror(@errmsg, @errsev, @errstate);
+end catch
+
+go
---
+++ auth-poc.data/scripts/implementation/dbo.UserAccount_insert.sql
@@ -0,0 +1,53 @@
+﻿/*
+Description:	INSERT Table dbo.UserAccount
+Author:			MeddlerOnTheRoof
+Date:			May 23, 2017
+
+Role Description
+admin				:: can do everything all other users can and perform CRUD operations on users (also delete rights on others' records)
+content_creator		:: fulls rights to records they've created and rights to edit records others have created
+content_contributor	:: full rights to content they've created and rights to consume but not modify records others have created
+content_consumer	:: rights to view all content but not create, update, or delete
+*/
+
+begin try
+	use authpoc;
+
+	if not exists(select 1 from dbo.UserAccount)
+		begin
+			declare @user char(8);
+			declare @date datetime;
+
+			set @user = 'system';
+			set @date = getdate();
+
+			set identity_insert dbo.UserAccount on;
+
+			insert into dbo.UserAccount (UserAccountId, UserAccountName, UserAccountPassword, UserRoleId, CreatedByUser, CreatedByDate, ModifiedByUser, ModifiedByDate)
+			values 
+				(1, 'admin', 'password', 1, @user, @date, @user, @date),
+				(2, 'content_creator_example', 'password', 2, @user, @date, @user, @date),
+				(3, 'content_contributor_example', 'password', 3, @user, @date, @user, @date),
+				(4, 'content_consumer_example', 'password', 4, @user, @date, @user, @date)
+
+			set identity_insert dbo.UserAccount off;
+
+			print 'dbo.UserAccount populdated';
+		end;
+	else
+		print 'dbo.UserAccount was already populated';
+end try
+
+begin catch
+	declare @errmsg nvarchar(2048);
+	declare @errsev int;
+	declare @errstate int;
+
+	set @errmsg = 'Error: ' + error_message();
+	set @errsev = error_severity();
+	set @errstate = error_state();
+
+	raiserror(@errmsg, @errsev, @errstate);
+end catch
+
+go
