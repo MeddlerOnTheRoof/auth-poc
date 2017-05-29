19d11ec68c43fa60139f0fc5871bd0d5265b20ba
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-29 14:02:34 -0400

    Add ArmorType, AttackType, and UserRole to lookup ts models

--- auth-poc.ui/ClientApp/app/models/lookup-models.ts
+++ auth-poc.ui/ClientApp/app/models/lookup-models.ts
@@ -1,6 +1,23 @@
 ﻿import { BaseEntity } from './base-entity';
 
+export class ArmorType extends BaseEntity {
+    public armorTypeId: number;
+    public armorTypeName: string;
+}
+
+export class AttackType extends BaseEntity {
+    public attackTypeId: number;
+    public attackTypeName: string;
+    public armorTypeId: number;
+    public armorTypeName: string;
+}
+
 export class UnitType extends BaseEntity {
     public unitTypeId: number;
     public unitTypeName: string;
+}
+
+export class UserRole extends BaseEntity {
+    public userRoleId: number;
+    public userRoleName: string;
 }
