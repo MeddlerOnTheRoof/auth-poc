b5fbbc6df76450f3c28f253fceb961752941fb24
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-05-30 08:38:34 -0400

    Add UnitArmor model

---
+++ auth-poc.data/Models/UnitArmor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace auth_poc.data.Models
+{
+    public class UnitArmor : BaseEntity
+    {
+        public int UnitArmorId { get; set; }
+        public int UnitId { get; set; }
+        public int ArmorTypeId { get; set; }
+        public string ArmorTypeName { get; set; }
+        public int UnitArmorValue { get; set; }
+    }
+}
