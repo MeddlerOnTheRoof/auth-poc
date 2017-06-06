ee54670666c92edd865a2ee28c98915333d66cd8
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-06-06 08:44:17 -0400

    Add admin-dashboard.component

---
+++ auth-poc.ui/ClientApp/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'admin-dashboard',
+    template: require('./admin-dashboard.component.html')
+})
+
+export class AdminDashboardComponent {
+
+}
