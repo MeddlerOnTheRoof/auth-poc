d4c6f47f61a00b3ac5d3cb0f5996ea6a1feb6cb7
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-06-06 08:45:26 -0400

    Add editor.component

---
+++ auth-poc.ui/ClientApp/editor/components/editor/editor.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'editor',
+    template: require('./editor.component.html'),
+    styles: [require('./editor.component.css')]
+})
+
+export class EditorComponent {
+
+}
