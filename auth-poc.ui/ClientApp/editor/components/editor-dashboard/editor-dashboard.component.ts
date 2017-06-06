6fc6e45f71eaaa98d2e4ea91765a7e200ce607e3
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-06-06 08:45:37 -0400

    Add editor-dashboard.component

---
+++ auth-poc.ui/ClientApp/editor/components/editor-dashboard/editor-dashboard.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'editor-dashboard',
+    template: require('./editor-dashboard.component.html')
+})
+
+export class EditorDashboardComponent {
+
+}
