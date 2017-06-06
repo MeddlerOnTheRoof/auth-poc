5b52a92b9d0c8e3890642ef1e43dd2e5995a6f16
MeddlerOnTheRoof <schanne.mj@gmail.com>
2017-06-06 08:48:24 -0400

    Modify app.module to include new modules

--- auth-poc.ui/ClientApp/app/app.module.ts
+++ auth-poc.ui/ClientApp/app/app.module.ts
@@ -2,33 +2,53 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
-import { AppComponent } from './components/app/app.component'
+
+import { AdminModule } from '../admin/admin.module';
+import { AdminRoutingModule } from '../admin/admin-routing.module';
+import { AdminAuthGuard } from '../shared/admin-auth-guard';
+
+import { EditorModule } from '../editor/editor.module';
+import { EditorRoutingModule } from '../editor/editor-routing.module';
+import { EditorAuthGuard } from '../shared/editor-auth-guard';
+
+import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { UnitService } from './components/unit/unit.service';
-import { UnitListComponent } from './components/unit/unit-list.component';
-import { UnitDetailComponent } from './components/unit/unit-detail.component';
+import { UnuathorizedComponent } from './components/login/unauthorized.component';
+
+import { LoginComponent } from './components/login/login.component';
+
+import { TokenService } from '../shared/token.service';
 
 @NgModule({
-    bootstrap: [ AppComponent ],
+    bootstrap: [AppComponent],
     declarations: [
         AppComponent,
-        NavMenuComponent,
         HomeComponent,
-        UnitListComponent,
-        UnitDetailComponent
+        LoginComponent,
+        NavMenuComponent,
+        UnuathorizedComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        AdminModule,
+        AdminRoutingModule,
+        EditorModule,
+        EditorRoutingModule,
         FormsModule,
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent },
             { path: 'home', component: HomeComponent },
-            { path: 'unit-list', component: UnitListComponent },
-            { path: 'unit-detail', component: UnitDetailComponent },
+            { path: 'unauthorized', component: UnuathorizedComponent },
             { path: '**', redirectTo: 'home' }
         ])
+    ],
+    providers: [
+        TokenService,
+        AdminAuthGuard,
+        EditorAuthGuard
     ]
 })
-export class AppModule {
-}
+
+export class AppModule { }
